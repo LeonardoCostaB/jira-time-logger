d53636747e6b131f5dd6fe47ac4b6742cc733d95
Leonardo Costa <leonardo.costa.b@outlook.com>
2026-03-27 17:46:30 -0300

    feat: github actions job

--- server/api/cron/test.get.ts
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(() => {
-    console.log('Cron job test endpoint hit at', new Date().toISOString());
-    return {
-        statusCode: 200,
-        message: 'Cron job test endpoint hit successfully',
-    };
-});
